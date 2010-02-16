4104ca946b2f65b0c12f2a7fefbde2d23a73aed8
Mons Anderson <mons@cpan.org>
2010-02-16 19:47:04 +0300

    fix configure.ac for enable-threads

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 CFLAGS="-g -ggdb -I/usr/local/include/db47" LDFLAGS="-ldb-4.7 -L/usr/local/lib" ./configure \
+--enable-threads=yes \
 --prefix=$HOME --with-bdb=/usr/local --with-libevent=/usr/local
 # --enable-threads
